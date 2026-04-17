40deb64988081de77021563273ad7dc9bd71e381
ztf <982457331@qq.com>
2026-04-17 10:32:20 +0800

    feat: 启用异步支持用于审计日志异步写入

--- src/main/java/com/javaclaw/JavaclawApplication.java
+++ src/main/java/com/javaclaw/JavaclawApplication.java
@@ -4,9 +4,11 @@ import com.javaclaw.config.Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableConfigurationProperties(Config.class)
+@EnableAsync
 public class JavaclawApplication {
     public static void main(String[] args) {
         SpringApplication.run(JavaclawApplication.class, args);
